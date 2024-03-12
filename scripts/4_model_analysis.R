2319b130f08ab056b1da12797b4fcdd993b7983f
Sheena Tan <sheenatan2024@u.northwestern.edu>
2024-03-12 11:48:56 -0500

    analyzed tuned models

--- scripts/4_model_analysis.R
+++ scripts/4_model_analysis.R
@@ -14,59 +14,101 @@ load(here("data/spotify_train.rda"))
 # load models ----
 load(here("results/baseline_tuned.rda"))
 load(here("results/lm_fit.rda"))
-load(here("results/svm_tuned.rda"))
 load(here("results/knn_tuned.rda"))
 load(here("results/rf_tuned.rda"))
 load(here("results/bt_tuned.rda"))
+load(here("results/nnet_tuned.rda"))
 
 
-# hyperparameter analysis
-autoplot(svm_tuned)
-select_best(svm_tuned, metric = "accuracy")
+##########################################################################
+# Hyperparameter analysis ----
+##########################################################################
 
 autoplot(knn_tuned)
-select_best(knn_tuned, metric = "accuracy")
-
 autoplot(rf_tuned)
-select_best(rf_tuned, metric = "accuracy")
-
 autoplot(bt_tuned)
-select_best(bt_tuned, metric = "accuracy")
+autoplot(nnet_tuned)
 
+# function to show best performing parameter results
+show_best_params <- function(model_tuned, metric_string) {
+  show_best(model_tuned, metric = {{metric_string}}) |>
+    slice(1) |>
+    select(!c(.estimator, .config, .metric, n)) |>
+    rename(accuracy = mean) |>
+    pivot_longer(
+      cols = !c("accuracy", "std_err"),
+      names_to = "parameter",
+      values_to = "value"
+    )
+}
 
-## collect metrics across all folds for each model ----
+# bind parameters into one table
+table_best_params <- show_best_params(nnet_tuned, "accuracy") |>
+  mutate(model = "nnet", .before = "accuracy") |>
+  bind_rows(
+    show_best_params(rf_tuned, "accuracy") |>
+      mutate(model = "rf", .before = "accuracy")
+  ) |>
+  bind_rows(
+    show_best(lm_fit, metric = "accuracy") |>
+      select(mean, std_err) |>
+      rename(accuracy = mean) |>
+      mutate(model = "lm", .before = "accuracy")
+  ) |>
+  bind_rows(
+    show_best(baseline_tuned, metric = "accuracy") |>
+      select(mean, std_err) |>
+      rename(accuracy = mean) |>
+      mutate(model = "baseline", .before = "accuracy")
+  ) |>
+  bind_rows(
+    show_best_params(bt_tuned, "accuracy") |>
+      mutate(model = "bt", .before = "accuracy")
+  ) |>
+  bind_rows(
+    show_best_params(knn_tuned, "accuracy") |>
+      mutate(model = "knn", .before = "accuracy")
+  ) |> arrange(desc(accuracy))
+
+# write out parameter table
+save(table_best_params, file = here("results/table_best_params.rda"))
+
+##########################################################################
+# Model analysis ----
+##########################################################################
+
 metrics1 <- collect_metrics(baseline_tuned) |>
   mutate(model = "baseline") |>
-  select(model, .metric, mean, std_err)
+  select(model, .metric, mean, std_err, n)
 
 metrics2 <- collect_metrics(lm_fit) |>
   mutate(model = "lm") |>
-  select(model, .metric, mean, std_err)
+  select(model, .metric, mean, std_err, n)
 
-# metrics3 <- collect_metrics(bt_tuned) |>
-#   mutate(model = "svm") |>
-#   select(model, .metric, mean, std_err)
+metrics3 <- collect_metrics(nnet_tuned) |>
+  mutate(model = "nnet") |>
+  select(model, .metric, mean, std_err, n)
 
 metrics4 <- collect_metrics(knn_tuned) |>
   mutate(model = "knn") |>
-  select(model, .metric, mean, std_err)
+  select(model, .metric, mean, std_err, n)
 
 metrics5 <- collect_metrics(rf_tuned) |>
   mutate(model = "rf") |>
-  select(model, .metric, mean, std_err)
+  select(model, .metric, mean, std_err, n)
 
 metrics6 <- collect_metrics(bt_tuned) |>
   mutate(model = "bt") |>
-  select(model, .metric, mean, std_err)
+  select(model, .metric, mean, std_err, n)
 
 # bind metrics into one table
-train_metrics <-
-  bind_rows(metrics1, metrics2, metrics4, metrics5, metrics6) |>
+table_best_means <-
+  bind_rows(metrics1, metrics2, metrics3, metrics4, metrics5, metrics6) |>
   filter(.metric == "accuracy") |>
   group_by(model) |>
-  summarize(mean = mean(mean), std_err = mean(std_err)) |>
+  summarise(mean = mean(mean), std_err = mean(std_err), n = mean(n)) |>
   arrange(desc(mean))
 
-# write out metrics table
-save(train_metrics, file = here("results/train_metrics.rda"))
+# write out means table
+save(table_best_means, file = here("results/table_best_means.rda"))
 
