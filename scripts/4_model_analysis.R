b7ef650d0766656e31faca49843ae83936243e19
Sheena Tan <sheenatan2024@u.northwestern.edu>
2024-03-13 23:32:16 -0500

    final report written

--- scripts/4_model_analysis.R
+++ scripts/4_model_analysis.R
@@ -25,9 +25,15 @@ load(here("results/nnet_tuned.rda"))
 ##########################################################################
 
 autoplot(knn_tuned)
-autoplot(rf_tuned)
-autoplot(bt_tuned)
-autoplot(nnet_tuned)
+
+rf_params_autoplot <- autoplot(rf_tuned)
+ggsave(rf_params_autoplot, file = here("images/rf_params_autoplot.png"))
+
+bt_params_autoplot <- autoplot(bt_tuned)
+ggsave(bt_params_autoplot, file = here("images/bt_params_autoplot.png"))
+
+nnet_params_autoplot <- autoplot(nnet_tuned)
+ggsave(nnet_params_autoplot, file = here("images/nnet_params_autoplot.png"))
 
 # function to show best performing parameter results
 show_best_params <- function(model_tuned, metric_string) {
@@ -56,7 +62,7 @@ table_best_params <- show_best_params(nnet_tuned, "accuracy") |>
       mutate(model = "lm", .before = "accuracy")
   ) |>
   bind_rows(
-    show_best(baseline_fit, metric = "accuracy") |>
+    show_best(baseline_tuned, metric = "accuracy") |>
       select(mean, std_err) |>
       rename(accuracy = mean) |>
       mutate(model = "baseline", .before = "accuracy")
@@ -77,36 +83,36 @@ save(table_best_params, file = here("results/table_best_params.rda"))
 # Model analysis ----
 ##########################################################################
 
-metrics1 <- collect_metrics(baseline_fit) |>
+metrics1 <- collect_metrics(baseline_tuned) |>
   mutate(model = "baseline") |>
-  select(model, .metric, mean, std_err, n)
+  select(model, .metric, mean, std_err)
 
 metrics2 <- collect_metrics(lm_fit) |>
   mutate(model = "lm") |>
-  select(model, .metric, mean, std_err, n)
+  select(model, .metric, mean, std_err)
 
 metrics3 <- collect_metrics(nnet_tuned) |>
   mutate(model = "nnet") |>
-  select(model, .metric, mean, std_err, n)
+  select(model, .metric, mean, std_err)
 
 metrics4 <- collect_metrics(knn_tuned) |>
   mutate(model = "knn") |>
-  select(model, .metric, mean, std_err, n)
+  select(model, .metric, mean, std_err)
 
 metrics5 <- collect_metrics(rf_tuned) |>
   mutate(model = "rf") |>
-  select(model, .metric, mean, std_err, n)
+  select(model, .metric, mean, std_err)
 
 metrics6 <- collect_metrics(bt_tuned) |>
   mutate(model = "bt") |>
-  select(model, .metric, mean, std_err, n)
+  select(model, .metric, mean, std_err)
 
 # bind metrics into one table
 table_best_means <-
   bind_rows(metrics1, metrics2, metrics3, metrics4, metrics5, metrics6) |>
   filter(.metric == "accuracy") |>
   group_by(model) |>
-  summarise(mean = mean(mean), std_err = mean(std_err), n = mean(n)) |>
+  summarise(mean = mean(mean), std_err = mean(std_err)) |>
   arrange(desc(mean))
 
 # write out means table
@@ -121,8 +127,8 @@ library(gt)
 
 table_best_params <- table_best_params |>
   gt(rowname_col = "model") |>
-  fmt_number(columns = accuracy, decimals = 4) |>
-  fmt_scientific(columns = std_err, decimals = 3) |>
+  fmt_number(columns = accuracy, decimals = 5) |>
+  fmt_scientific(columns = std_err, decimals = 4) |>
   fmt_integer(columns = value, rows = c(1:5, 7:11)) |>
   tab_header(
     title = md("**Best performing model results**"),
@@ -136,13 +142,13 @@ table_best_params <- table_best_params |>
 
 table_best_means <- table_best_means |>
   gt(rowname_col = "model") |>
-  fmt_number(columns = mean, decimals = 4) |>
-  fmt_scientific(columns = std_err, decimals = 3) |>
+  fmt_number(columns = mean, decimals = 5) |>
+  fmt_scientific(columns = std_err, decimals = 4) |>
   tab_header(
-    title = md("**Mean model accuracy across folds**"),
+    title = md("**Mean model accuracy across parameters**"),
     subtitle = md("**Logistic** and **random forest** models performed best.")
   ) |>
   tab_stubhead(label = "model")
 
-gtsave(table_best_params, here("results/table_best_params.html"))
-gtsave(table_best_means, here("results/table_best_means.html"))
+gtsave(table_best_params, here("images/table_best_params.png"))
+gtsave(table_best_means, here("images/table_best_means.png"))
